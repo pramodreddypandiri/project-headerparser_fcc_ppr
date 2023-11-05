84f299ea8fdf2a3d058fbac83587517fd728a99d
pramodreddypandiri <98374193+pramodreddypandiri@users.noreply.github.com>
2023-11-05 17:05:42 +0000

    done

--- index.js
+++ index.js
@@ -23,7 +23,13 @@ app.get('/', function (req, res) {
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
+app.get('/api/whoami', function(req, res){
+  const ip = req.headers['x-forwarded-for'];
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({ipaddress: ip, language: language, software: software});
 
+});
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
